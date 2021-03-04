8d95ece4a8aa4e82577668c997ad235904e3ecbb
kwangyel <kwangyel@gmail.com>
2021-03-04 12:48:11 +0600

    member route bug

--- api/server/routes/memberRoutes.js
+++ api/server/routes/memberRoutes.js
@@ -4,7 +4,7 @@ import memberController from '../controllers/memberController';
 const router=Router();
 
 router.post('/create-bulk',memberController.createBulk)
-router.get('/get-all/:sid',memberController.retrieveHid)
+router.get('/get-all/:hid',memberController.retrieveHid)
 router.get('/get/:id',memberController.retrieve)
 router.patch('/update',memberController.update)
 router.delete('/delete/:id',memberController.delete)
