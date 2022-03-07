bc42c90187151120f66978708551b9c9a4dc44ae
kinley wangyel <kwangyel@gmail.com>
2022-03-07 09:47:31 +0600

    delete method changed to post

--- api/server/routes/redmemberRoutes.js
+++ api/server/routes/redmemberRoutes.js
@@ -6,6 +6,6 @@ const router=Router();
 router.post('/create',redmemberController.create)
 router.get('/get-all/:id',redmemberController.retrieveByFlatId)
 router.patch('/update',redmemberController.update)
-router.delete('/delete',redmemberController.delete)
+router.post('/delete',redmemberController.delete)
 
 export default router;
